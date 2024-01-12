d2781e5807be98b92e19415a7e40d14c4a45345e
YuriiVolodarskyi <alimych65@gmail.com>
2024-01-12 11:59:52 +0200

    Add Registration tests

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -4,6 +4,8 @@ import models.User;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HelperUser extends HelperBase {
     public HelperUser(WebDriver wd) {
@@ -17,7 +19,9 @@ public class HelperUser extends HelperBase {
         click(By.xpath("//a[text()='LOGIN']"));
     }
 
-    public void fillLoginForm(String email, String password) {
+
+
+    public void fillLoginRegistrationForm(String email, String password) {
         // WebElement emailInput = wd.findElement(By.xpath("//input[@placeholder='Email']"));
         // emailInput.click();
         // emailInput.clear();
@@ -29,6 +33,11 @@ public class HelperUser extends HelperBase {
         //  passwordInput.clear();
         // passwordInput.sendKeys(password);
         type(By.xpath("//input[last()]"), password);
+    }
+
+    public void fillLoginRegistrationForm(User user) {
+        type(By.xpath("//input[@placeholder='Email']"), user.getEmail());
+        type(By.xpath("//input[last()]"), user.getPassword());
     }
 
     public void submitLogin() {
@@ -52,8 +61,11 @@ public class HelperUser extends HelperBase {
         //return isElementPresent(By.className("contact-page_message__2qafk"));
     }
 
-    public void fillRegistrationForm(User user) {
-        type(By.xpath("//input[@placeholder='Email']"), user.getEmail());
-        type(By.xpath("//input[last()]"), user.getPassword());
+    public boolean isNoContactsHereDisplayed() {
+        WebDriverWait wait = new WebDriverWait(wd,5);
+        return wait.until(ExpectedConditions.textToBePresentInElement
+                (wd.findElement(By.cssSelector(".contact-page_message__2qafk>h1")),
+                        "No Contacts here!"));
     }
+
 }
--- src/test/java/tests/RegistrationTests.java
+++ src/test/java/tests/RegistrationTests.java
@@ -24,10 +24,51 @@ public class RegistrationTests extends TestBase {
                 .withEmail("volodar" + z + "@gmail.com")
                 .withPassword("Vvolodar12345$");
         app.getHelperUser().openLoginRegistrationForm();
-        app.getHelperUser().fillRegistrationForm(user);
+        app.getHelperUser().fillLoginRegistrationForm(user);
         app.getHelperUser().submitRegistration();
 
         Assert.assertTrue(app.getHelperUser().isRegistered());
+        //Assert.assertTrue(app.getHelperUser().isNoContactsHereDisplayed());
+
+    }
+
+    //@Test(description = "Bug report N ***", enabled = false)
+    @Test
+    public void registrationWrongEmail() {
+        User user = new User()
+                .withEmail("volodargmail.com")
+                .withPassword("Vvolodar12345$");
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm(user);
+        app.getHelperUser().submitRegistration();
+
+        Assert.assertTrue(app.getHelperUser().isAlertPresent("Wrong email or password format"));
+
+    }
+
+    @Test
+    public void registrationWrongPassword() {
+        User user = new User()
+                .withEmail("volodar@gmail.com")
+                .withPassword("2345$");
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm(user);
+        app.getHelperUser().submitRegistration();
+
+        Assert.assertTrue(app.getHelperUser().isAlertPresent("Wrong email or password format"));
+
+    }
+
+    @Test
+    public void registrationExistUser() {
+        User user = new User()
+                .withEmail("alimych65@gmail.com")
+                .withPassword("Yv030665@");
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm(user);
+        app.getHelperUser().submitRegistration();
+
+        Assert.assertTrue(app.getHelperUser().isAlertPresent("User already exist"));
 
     }
 
