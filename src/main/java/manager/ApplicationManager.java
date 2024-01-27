442b89af2cee782ac9d780aff9ae969ba7ecfe5d
YuriiVolodarskyi <alimych65@gmail.com>
2024-01-27 16:58:08 +0200

    Add listeners

--- src/main/java/manager/ApplicationManager.java
+++ src/main/java/manager/ApplicationManager.java
@@ -2,6 +2,7 @@ package manager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,13 +10,13 @@ import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
     Logger logger = LoggerFactory.getLogger(ApplicationManager.class);
-    WebDriver wd;
-
+    //WebDriver wd;
+    EventFiringWebDriver wd;
     HelperUser helperUser;
     HelperContact helperContact;
 
     public void init() {
-        wd = new ChromeDriver();
+        wd = new EventFiringWebDriver(new ChromeDriver());
         logger.info("All tests run in Chrome Browser");
         wd.manage().window().maximize();
         wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
@@ -23,6 +24,7 @@ public class ApplicationManager {
         logger.info("The link --> " + wd.getCurrentUrl());
         helperUser = new HelperUser(wd);
         helperContact = new HelperContact(wd);
+        wd.register(new ListenerWD());
     }
 
     public HelperUser getHelperUser() {
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -3,6 +3,7 @@ package tests;
 import manager.ApplicationManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
@@ -27,5 +28,10 @@ public class TestBase {
     @AfterSuite
     public void tearDown() {
         //app.stop();
+    }
+
+    @AfterMethod
+    public void end(){
+        logger.info("==============================================");
     }
 }
